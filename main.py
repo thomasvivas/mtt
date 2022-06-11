1c323f9cc74aeb91cd6bd56b910cb873d9dbee82
Thomas Vivas <thomas.p.vivas@gmail.com>
2022-06-11 19:19:41 -0400

    handle media files with certain extensions to certain functions for video playing and image rendering

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from enum import Enum
+import sys
 
 IMAGE_TARGET = "target.jpg"
 
@@ -29,9 +31,41 @@ def print_image(file_name: str) -> None:
             print_pixel(image.getpixel((column, row)))
         print()
 
+def play_video(file_name: str):
+    # Placeholder for video playing implementation
+    pass
+
+class FileType(Enum):
+    UNSUPPORTED = 1
+    IMAGE = 2
+    VIDEO = 3
+
+def file_type(file_name: str) -> FileType:
+    EXTENSION = file_name.split(".")[-1]
+    match EXTENSION:
+        case "jpg" | "jpeg" | "png":
+            return FileType.IMAGE
+        case "mp4" | "mov":
+            return FileType.VIDEO
+        case _:
+            return FileType.UNSUPPORTED
+
 
 def main() -> None:
-    print_image("sample.jpg")
+    if len(sys.argv) == 1:
+        raise LookupError("No file was supplied")
+
+    SELECTED_FILE = sys.argv[1]
+    SELECTED_TYPE = file_type(SELECTED_FILE)
+
+    if SELECTED_TYPE is FileType.UNSUPPORTED:
+        raise ValueError("File type is not supported")
+    
+    match SELECTED_TYPE:
+        case FileType.IMAGE:
+            print_image(SELECTED_FILE)    
+        case FileType.VIDEO:
+            play_video(SELECTED_FILE)
 
 if __name__ == "__main__":
     main()
